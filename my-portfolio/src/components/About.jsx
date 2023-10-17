010a5eca5bdb3980b9475c48b73e6d75895b3e3b
sahlairsha <fsahla370@gmail.com>
2023-10-17 15:46:56 +0530

    fix

--- my-portfolio/src/components/About.jsx
+++ my-portfolio/src/components/About.jsx
@@ -36,7 +36,7 @@ const About = () => {
     </motion.div>
     <motion.p variants={fadeIn("","", 0.1, 1)} className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'>
       I'm a skilled Front-end developer with experience in JavaScript,Html5,Css and experience in framework like React.js,Node.js. 
-      I'm a qucik learner and collaborate closely with clients to create efficient,scalable and user-friendly solutions that solve real world problems.
+      I'm a quick learner and collaborate closely with clients to create efficient,scalable and user-friendly solutions that solve real world problems.
       Let's work together to bring your ideas to life.
     </motion.p>
     <div className='mt-20 flex flex-wrap gap-10'>
